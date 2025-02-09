121d7fb75da87ceeeabd5a227e2720016310e799
denis <bluesky171@mail.ru>
2025-02-10 00:24:39 +0300

    create project

---
+++ CDProject/CDProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CDProject
+//
+//  Created by Денис Ефименков on 10.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .yellow
+    }
+
+
+}
+
